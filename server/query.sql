02a2f42144b9480a58af79c4d1ae8c921b290384
ericafenyo <ericafenyo@yahoo.com>
2019-08-05 07:46:10 +0200

    add base sql query

---
+++ server/query.sql
@@ -0,0 +1,28 @@
+-- Create the Database.
+CREATE DATABASE tasti;
+
+-- Use the newly created database.
+USE tasti;
+
+-- Create tables
+
+CREATE TABLE `user`(
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `biography` TEXT,
+  `email` varchar(255),
+  `phone` INT,
+  `image` blob,
+  `recipe_id` INT,
+  FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
+);
+
+CREATE TABLE `recipe`(
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `recipe`(
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL
+);
