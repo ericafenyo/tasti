07af115ba6907ff3e72c3d932c3d85d85f8b6f07
ericafenyo <ericafenyo@yahoo.com>
2019-08-08 07:14:51 +0200

    create Input component

---
+++ app/src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui-input">
+    <label for="input-text" class="ui-input-label">
+      <input @input="handleInput" v-model="value" type="text" id="input-text" class="ui-input-input" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  method:{
+    
+  }
+};
+</script>
+
