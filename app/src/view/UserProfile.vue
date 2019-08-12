ec7ab6178067b06bebb39936bace3c0b4d0361b7
ericafenyo <ericafenyo@yahoo.com>
2019-08-12 18:19:32 +0200

    added user profile component

---
+++ app/src/view/UserProfile.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="profile">
+    <div class="avatar-wrapper">
+      <img src alt="avatar" />
+    </div>
+    <div class="profile-info">
+      <h2>Nick Evans</h2>
+      <p>Potato Master</p>
+      <div class="detail">
+        <span>584 followers</span>
+        <span>23k likes</span>
+      </div>
+    </div>
+
+    <div class="recipes" v-for="(recipe, index) in recipes" key="index">
+        <Recipe recipe = recipes[index]/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      recipes: [
+        {
+          name: "some name",
+          image: ""
+        },
+        {
+          name: "some name",
+          image: ""
+        }
+      ]
+    };
+  },
+
+  component: {
+    name: ""
+  }
+};
+</script>
+
+
+
