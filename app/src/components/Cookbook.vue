ebc40f5d6cdaaf29e5d61a8e1068ae746cc7e279
ericafenyo <ericafenyo@yahoo.com>
2019-08-13 07:29:27 +0200

    rename file

---
+++ app/src/components/Cookbook.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cookbook">
+    <div class="cookbook-thumbnail">
+      <img
+        class="image-fluid"
+        src="https://images.unsplash.com/photo-1496174883999-edcc585a373f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+        alt="recipe thumbnail"
+      />
+    </div>
+    <p class="cookbook-label">Sweets</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.cookbook {
+
+}
+</style>
