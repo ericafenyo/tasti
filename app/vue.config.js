11065c5cb09705e61249adcdd5196a59e60b18ad
ericafenyo <ericafenyo@yahoo.com>
2019-08-16 02:49:13 +0200

    add config for svg loader

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `@import "@/scss/_variables.scss";`
-      }
-    }
-  }
+	css: {
+		loaderOptions: {
+			sass: {
+				data: `
+          @import "@/scss/index.scss";
+        `
+			}
+		}
+	},
+	chainWebpack: (config) => {
+		const svgRule = config.module.rule('svg');
+
+		svgRule.uses.clear();
+
+		svgRule.use('vue-svg-loader').loader('vue-svg-loader');
+	}
 };
