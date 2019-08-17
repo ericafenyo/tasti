8c673b5f90c59f6fcf72c240a871626b3cb1105a
ericafenyo <ericafenyo@yahoo.com>
2019-08-17 18:16:29 +0200

    update database dump file

--- server/dump.sql
+++ server/dump.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.16, for osx10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: tasti
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	5.7.27-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -20,16 +20,16 @@
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `direction` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `video` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  `content` text NOT NULL,
   `recipe_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `recipe_id` (`recipe_id`),
   CONSTRAINT `direction_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,16 +42,16 @@ CREATE TABLE `direction` (
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `serving_time` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `nutrition_facts` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `serving_time` varchar(255) NOT NULL,
+  `nutrition_facts` varchar(255) NOT NULL,
   `recipe_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `recipe_id` (`recipe_id`),
   CONSTRAINT `info_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,65 +64,76 @@ CREATE TABLE `info` (
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ingredient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `recipe_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `recipe_id` (`recipe_id`),
   CONSTRAINT `ingredient_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ingredient`
 --
 
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (1,'2 teaspoons ground cumin',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (2,'1/4 teaspoon ground turmeric',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (3,'1.2kg Lilydale whole chicken, cut into 8 pieces',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (4,'60ml (1⁄4 cup) extra virgin olive oil',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (5,'2 brown onions, cut into thin wedges',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (6,'2 garlic cloves, crushed',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (7,'1 cinnamon stick',1);
+INSERT INTO `ingredient` (`id`, `name`, `recipe_id`) VALUES (8,'120g drained green Sicilian olives',1);
 
 --
 -- Table structure for table `recipe`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `recipe` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `recipe_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `recipe`
 --
 
+INSERT INTO `recipe` (`id`, `name`, `user_id`) VALUES (1,'Moroccan chicken and rice one-pot',2);
 
 --
 -- Table structure for table `user`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `biography` text COLLATE utf8mb4_general_ci,
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `biography` text,
   `phone` int(11) DEFAULT NULL,
-  `image` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `biography`, `phone`, `image`) VALUES (1,'John Doe','john.doe@example.com','f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9',NULL,NULL,NULL);
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `biography`, `phone`, `image`) VALUES (2,'Debra G Howard','debra@example.com','73d5b2f4ba82d59c723c16a909524559d8f31e33c5d8fdcfc57065dca5c9f189',NULL,NULL,NULL);
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +144,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-17 12:21:02
+-- Dump completed on 2019-08-17 18:14:46
