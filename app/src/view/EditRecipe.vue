ad19dfd1f9ff190000148f53089bcc735f9fe12b
ericafenyo <ericafenyo@yahoo.com>
2019-08-20 17:46:28 +0200

    code update

---
+++ app/src/view/EditRecipe.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-modal>
+    <div>Some content</div>
+  </v-modal>
+</template>
+
+<script>
+import Modal from "../components/Modal";
+export default {
+  components: {
+    "v-modal": Modal
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
