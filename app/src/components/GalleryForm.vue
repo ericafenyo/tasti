5635becc380dbd97e66eba86892938b2d4dfdb1a
ericafenyo <ericafenyo@yahoo.com>
2019-08-22 09:35:49 +0200

    add new Component

---
+++ app/src/components/GalleryForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="edit-gallery">
+    <div class="header">
+      <div class="header-title"></div>
+      <div class="header-icon">
+        <icon-edit />
+      </div>
+    </div>
+
+    <div class="carousel">
+      <div class="carousel-header">
+        <p>Images(8)</p>
+        <div class="carousel-icon"></div>
+      </div>
+      <div class="carousel-content">
+        <div class="image-gallery">
+          <div class="gallery-item">
+            <img src alt />
+          </div>
+        </div>
+      </div>
+
+      <div class="radio-button">
+        <input type="radio" />
+        <span>Set as Cover</span>
+      </div>
+
+      <div class="icon-label">
+        <icon-edit />
+        <span>Remove</span>
+      </div>
+
+      <div class="upload-image">Uplaod Images</div>
+
+      <button class="button">Save Gallery</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
