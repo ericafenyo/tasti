e8dbf17f78f0c3ae4364ce6df7543e36edbb23eb
ericafenyo <ericafenyo@yahoo.com>
2019-08-31 22:34:51 +0200

    genral code clean up

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from '@storybook/vue';
+import { configure, addDecorator, addParameters } from '@storybook/vue';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import Vue from 'vue';
 import { extend, ValidationProvider } from 'vee-validate';
 import * as rules from 'vee-validate/dist/rules';
 import en from 'vee-validate/dist/locale/en';
 import '../src/scss/index.scss';
+
+addParameters({
+	docs: {
+		container: DocsContainer,
+		page: DocsPage
+	}
+});
 
 // loop over all rules
 for (let rule in rules) {
@@ -16,8 +24,8 @@ for (let rule in rules) {
 // Register Component globally
 Vue.component('ValidationProvider', ValidationProvider);
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /\.stories\.js$/);
+// automatically import all files ending in *.stories.js and *.stories.mdx
+const req = require.context('../src', true, /\.stories\.(js|mdx)$/);
 function loadStories() {
 	req.keys().forEach((filename) => req(filename));
 }
