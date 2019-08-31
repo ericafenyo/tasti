c7a6fdfec368f5adcc3f4b96ccd01c64930a05a0
ericafenyo <ericafenyo@yahoo.com>
2019-08-31 15:19:19 +0200

    add class

--- app/src/components/Headline/Headline.vue
+++ app/src/components/Headline/Headline.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BaseHeadline :level="level" class="headline">{{text}}</BaseHeadline>
+    <BaseHeadline :level="level" class="headline" :class="`headline-${level}`">{{text}}</BaseHeadline>
   </div>
 </template>
 
@@ -30,6 +30,30 @@ export default {
 
 <style lang="scss" scoped>
 .headline {
-  font-family: $font;
+  &-1 {
+    font-size: 2.5rem;
+    font-weight: 300;
+    letter-spacing: -0.025em;
+    color: rgba(19, 41, 63, 0.65);
+    line-height: 1;
+  }
+  &-2 {
+    line-height: 1.06667;
+    letter-spacing: -0.025em;
+    font-size: 1.875rem;
+    color: rgba(19, 41, 63, 0.65);
+    font-weight: 300;
+    line-height: 1.06667;
+  }
+
+  &-3 {
+    font-size: 1.375rem;
+    color: #13293f;
+    line-height: 1.18182;
+    font-weight: 400;
+    letter-spacing: -0.015em;
+    color: #13293f;
+    line-height: 1.18182;
+  }
 }
 </style>
