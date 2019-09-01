47f6ee068d3a94c05625dbef7a5ef6534d4c5fc9
ericafenyo <ericafenyo@yahoo.com>
2019-09-01 09:39:59 +0200

    Add link

---
+++ app/src/components/Link/Link.vue
@@ -0,0 +1,24 @@
+<template>
+  <a class="link" :class="`link-${type}`" href>{{text}}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "Link"
+    },
+    type: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.link {
+  
+}
+</style>
