f1e4be543cd203f07eec68752514fea8884f0758
ericafenyo <ericafenyo@yahoo.com>
2019-09-18 13:37:28 +0200

    improvement: convert to class component

--- app/src/components/Text/Text.vue
+++ app/src/components/Text/Text.vue
@@ -1,17 +1,32 @@
 <template>
-  <p>{{text}}</p>
+  <p class="text" :class="composeClass">{{text}}</p>
 </template>
 
-<script>
-export default {
-  props: {
-    text: {
-      type: String,
-      default: "Text"
-    }
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+/**
+ *
+ */
+@Component
+export default class Text extends Vue {
+  // Props
+  @Prop({ type: String, default: "Text" }) readonly text!: string;
+  @Prop({ type: String, default: "" }) readonly size!: string;
+  @Prop({ type: String, default: "" }) readonly color!: string;
+
+  get composeClass(): String {
+    const { size } = this;
+    return size ? `text-${size}` : "";
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/_resources.scss";
+
+.text {
+  font-size: 1em;
+  
+  }
 </style>
