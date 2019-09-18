28351479e08c9ffc9867261ef662fc7b0e2b23a9
ericafenyo <ericafenyo@yahoo.com>
2019-09-18 13:35:14 +0200

    add a Subhead Component

---
+++ app/src/components/Subhead/Subhead.vue
@@ -0,0 +1,32 @@
+<template>
+  <h4 class="subhead" :class="computeClass">{{text}}</h4>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Subhead extends Vue {
+  @Prop({ type: String, default: "Subhead" }) readonly text!: string;
+  @Prop({ type: String, default: "" }) readonly size!: string;
+
+  /**
+   *
+   */
+  get computeClass(): String {
+    const { size } = this;
+    return size ? `subhead-${size}` : "";
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_resources.scss";
+.subhead{
+  font-size: 1em;
+  font-weight: 700;
+  &-small{
+
+  }
+}
+</style>
