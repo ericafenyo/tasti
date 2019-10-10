1f66d5e634cf3ad1fd2ca932c377fdcec9c2defd
ericafenyo <ericafenyo@yahoo.com>
2019-10-10 07:08:59 +0200

    feat: add icon components

---
+++ app/src/components/Icons/IconBase.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title :id="iconName" lang="en">{{ iconName }} icon</title>
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: "edit"
+    },
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    },
+    iconColor: {
+      type: String,
+      default: "currentColor"
+    }
+  }
+};
+</script>
