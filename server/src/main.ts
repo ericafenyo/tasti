a536b20466d57eeb29826134e8a22ceb9a00f4c9
ericafenyo <ericafenyo@yahoo.com>
2019-11-16 21:25:07 +0100

    :zap: update server configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as helmet from 'helmet';
 require('dotenv').config();
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	app.use(helmet());
+	app.enableCors();
+	await app.listen(port, () => {
+		Logger.log(`Server running on http://localhost:${port}`);
+	});
 }
 bootstrap();
