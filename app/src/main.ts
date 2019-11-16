a61e84be3632ea77f235e778005ac064216b1069
ericafenyo <ericafenyo@yahoo.com>
2019-11-16 21:18:42 +0100

    :zap: configure form validators

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,39 +1,44 @@
-import Vue from "vue";
+import Vue from 'vue';
 
 // Vee-validate
-import { extend, ValidationProvider, ValidationObserver } from "vee-validate";
-import * as rules from "vee-validate/dist/rules";
-// import en from 'vee-validate/dist/locale/en.json';
-
+import { extend, ValidationProvider, ValidationObserver } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+const en = require('vee-validate/dist/locale/en.json');
+import Vuelidate from 'vuelidate';
+Vue.use(Vuelidate);
 // Styles
-import "../src/scss/index.scss";
-import "./scss/normalize.css";
-import "./scss/bootstrap-grid.css";
+import '../src/scss/index.scss';
+import './scss/normalize.css';
+import './scss/bootstrap-grid.css';
 
-import App from "./App.vue";
-import { store } from "./store";
+import App from './App.vue';
+import { store } from './store';
 
-//loop over all rules
-// for (let rule in rules) {
-// 	extend(rule, {
-// 		// ...rules[rule], // add the rule
-// 		// message: en.messages[rule] // add its message
-// 	});
-// }
+// Add the required rule
+extend('required', {
+	validate: required.validate,
+	message: (field) => `The ${field} field is required`
+});
+
+// Add the email rule
+extend('email', {
+	validate: email.validate,
+	message: 'The {_field_} field must be a valid email'
+});
 
 // Custom  vee-validate rule
-// extend('checkbox', {
-// 	validate: (value) => value
-// });
+extend('checkbox', {
+	validate: (value) => value
+});
 
 // // Register Component globally
-Vue.component("ValidationProvider", ValidationProvider);
-Vue.component("ValidationObserver", ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  el: "#app",
-  render: h => h(App),
-  store
+	el: "#app",
+	render: h => h(App),
+	store
 }).$mount("#app");
