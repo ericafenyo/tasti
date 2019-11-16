261b638e244af28f37dd2da816de100cf790bb99
ericafenyo <ericafenyo@yahoo.com>
2019-11-16 21:22:52 +0100

    :zap: add a data layer impl for user entity

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -1,26 +1,25 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
-import { user } from "../data/model/User";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { userRepository } from '@/data/Injector';
 
 Vue.use(Vuex);
 
 const initialState = {
-  users: [],
+	users: []
 };
 export const store = new Vuex.Store({
-  state: initialState,
+	state: initialState,
 
-  mutations: {
-    CREATE_USER(state, payload: object) {
-      // state.users.push(payload);
-    },
-  },
+	mutations: {
+		CREATE_USER(state, payload: object) {
+			// state.users.push(payload);
+		}
+	},
 
-  actions: {
-    createUser({ commit }, userInfo: object) {
-      user.signUp(userInfo, () => {});
-      commit("CREATE_USER", userInfo);
-    },
-  },
+	actions: {
+		createUser({ commit }, userInfo: object) {
+			userRepository.createUser(userInfo);
+			commit('CREATE_USER', userInfo);
+		}
+	}
 });
