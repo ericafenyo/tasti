250dc6dbcf7b9881bdd1f7ba8c6c9804d0f409ef
ericafenyo <ericafenyo@yahoo.com>
2019-11-17 19:52:20 +0100

    :zap: add a user authentication

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-	constructor(private userService: UserService, private jwtService: JwtService) {}
-	async validateUser(emailAddress: string, password: string) {
-		const user = await this.userService.findOne(emailAddress);
-		if (user && user.password === password) {
-			const { password, ...result } = user;
-			return result;
-		}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
-		return null;
-	}
+  async validateUser(username: string, password: string) {
+	const user = await this.userService.findOne(username);
+	console.log(bcrypt);
+	
+    const passwordMatched = await bcrypt.compare(password, user.password);
 
-	async login(user: any) {
-		const payload = { username: user.username, sub: user.userId };
-		return {
-			access_token: this.jwtService.sign(payload)
-		};
-	}
+    if (user && passwordMatched) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+
+    return {
+      access_token: this.jwtService.sign(payload)
+    };
+  }
 }
