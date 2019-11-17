3aa1f25502966de590ab7102fbed730d781a2d14
ericafenyo <ericafenyo@yahoo.com>
2019-11-17 14:33:17 +0100

    :zap: add form elements to component

--- app/src/views/Login/Login.vue
+++ app/src/views/Login/Login.vue
@@ -1,21 +1,114 @@
 <template>
-    <div class="login" >
-        
+  <section class="section-no-header background-surface">
+    <div class="login">
+      <div class="container">
+        <Headline text="Login To Your Account" :level="3" class="mb-4" />
+        <form @submit.prevent novalidate="true">
+          <div class="form-item">
+            <Input
+              label="Email Address"
+              type="text"
+              name="email"
+              :className="[{'input-error': $v.email.$error}]"
+              placeholder="name@example.com"
+              @on-input="onInput"
+            />
+          </div>
+          <div class="form-item">
+            <Input
+              label="Password"
+              type="password"
+              name="password"
+              :className="[{'input-error': $v.password.$error}]"
+              placeholder="Enter 8 or more characters"
+              @on-input="onInput"
+            />
+          </div>
+          <Button text="Login" class="mb-3" type="primary" @on-click="onSubmit" />
+        </form>
+      </div>
     </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Vue, Prop, Emit, Component } from "vue-property-decorator";
+import { Validate, Validations } from "vuelidate-property-decorators";
+import { required } from "vuelidate/lib/validators";
 
+import Headline from "@/components/Headline/Headline.vue";
 import Input from "@/components/Input/Input.vue";
 import Button from "@/components/Button/Button.vue";
 
-@Component
+@Component({
+  components: {
+    Headline,
+    Input,
+    Button
+  }
+})
 export default class Login extends Vue {
+  email: string = "";
+  password: string = "";
 
+  @Validations()
+  validations = {
+    email: { required },
+    password: { required }
+  };
+
+  onSubmit() {
+    const { $touch, $invalid } = this.$v;
+    // Force the validation of form
+    $touch();
+
+    if (!$invalid) {
+      //   this.$store.dispatch("createUser", this.formData);
+      //   this.$router.replace("login");
+    }
+  }
+
+  onInput({ value, name }) {
+    this[name] = value;
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "@/scss/_resources.scss";
+.login {
+  max-width: 560px;
+  margin: 0 auto;
+  background-color: $white;
+  padding: 2rem 0;
+  height: 100%;
 
+  @include phablet {
+    height: initial;
+  }
+
+  @include laptop {
+    padding: 3rem 1rem;
+    padding-top: 3rem;
+    margin-top: 3rem;
+    border: 1px solid $color-border;
+  }
+
+  .account-notice {
+    span {
+      color: $color-primary-text;
+    }
+
+    .action-sign-in {
+      color: #30be76;
+      font-size: 16px;
+      margin: 0 8px;
+    }
+  }
+
+  .button {
+    width: 100%;
+    margin-top: 1rem;
+  }
+}
 </style>
