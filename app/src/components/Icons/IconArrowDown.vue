931917a6c2e845935e195eb31273973f8f8594a0
ericafenyo <ericafenyo@yahoo.com>
2019-11-20 01:19:09 +0100

    :zap: add default import

--- app/src/components/Icons/IconArrowDown.vue
+++ app/src/components/Icons/IconArrowDown.vue
@@ -1,3 +1,8 @@
 <template>
   <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
--- app/src/components/Icons/IconArrowUp.vue
+++ app/src/components/Icons/IconArrowUp.vue
@@ -1,3 +1,8 @@
 <template>
   <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6 1.41 1.41z"/>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
