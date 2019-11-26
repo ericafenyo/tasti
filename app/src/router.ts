a310739958baa7e87c0cf7d68c43fedeef47de38
ericafenyo <ericafenyo@yahoo.com>
2019-11-26 08:13:05 +0100

    :zap: add route for creating new recipe

--- app/src/router.ts
+++ app/src/router.ts
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 import Login from './views/Login/Login.vue';
 import Register from './views/Register/Register.vue';
+import CreateRecipe from './views/CreateRecipe.vue';
 
 Vue.use(Router);
 export default new Router({
@@ -15,6 +16,10 @@ export default new Router({
     {
       path: '/signup',
       component: Register
+    },
+    {
+      path: '/recipes/new',
+      component: CreateRecipe
     }
   ]
 });
