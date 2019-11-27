89244a0744330dc66167cb504dae62e4561d73a9
ericafenyo <ericafenyo@yahoo.com>
2019-11-27 18:02:43 +0100

    :zap: add relationship to entities

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -1,18 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Recipe } from '../recipe/recipe.entity';
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('increment') 
+  @PrimaryGeneratedColumn('uuid') 
   id: number;
 
 	@Column({ name: 'first_name' })
-	firstName: string;
-
+  firstName: string;
+  
 	@Column({ name: 'last_name' })
 	lastName: string;
 
-  @Column() 
+  @Column({unique : true}) 
   email: string;
 
   @Column() 
   password: string;
+
+  @OneToMany(type => Recipe, recipe => recipe.user)
+  recipes: Recipe[];
 }
