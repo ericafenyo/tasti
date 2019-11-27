486fc5411da193a2fa17d7998518116a4c06f63c
ericafenyo <ericafenyo@yahoo.com>
2019-11-27 18:02:18 +0100

    :zap: update recipe crud operation

--- server/src/recipe/recipe.module.ts
+++ server/src/recipe/recipe.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recipe } from './recipe.entity';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  controllers: [RecipeController],
-  providers: [RecipeService]
+	imports: [
+		TypeOrmModule.forFeature([ Recipe ]),
+		MulterModule.register({
+			dest: './uploads',
+		}),
+	],
+	controllers: [ RecipeController ],
+	providers: [ RecipeService ],
 })
 export class RecipeModule {}
