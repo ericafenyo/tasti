a67fee944b6257b56074143a82ef5f3d15041905
ericafenyo <ericafenyo@yahoo.com>
2019-11-27 08:22:52 +0100

    :construction: start to define recipe crud methods

--- server/src/recipe/recipe.service.ts
+++ server/src/recipe/recipe.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Recipe } from './recipe.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  constructor(@InjectRepository(Recipe) private recipeRepository: Repository<Recipe>) {}
+  async create(recipe: Recipe) {
+      const createdRecipe = this.recipeRepository.create(recipe);
+      if (!createdRecipe) {
+          
+      }
+  }
+}
