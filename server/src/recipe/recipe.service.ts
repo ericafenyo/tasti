85ac2101570675f6ae3ff83bdd0cfb86526d4e61
ericafenyo <ericafenyo@yahoo.com>
2019-11-27 18:02:18 +0100

    :zap: update recipe crud operation

--- server/src/recipe/recipe.service.ts
+++ server/src/recipe/recipe.service.ts
@@ -1,15 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
 import { Recipe } from './recipe.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class RecipeService {
-  constructor(@InjectRepository(Recipe) private recipeRepository: Repository<Recipe>) {}
-  async create(recipe: Recipe) {
-      const createdRecipe = this.recipeRepository.create(recipe);
-      if (!createdRecipe) {
-          
-      }
-  }
+	constructor(@InjectRepository(Recipe) private recipeRepository: Repository<Recipe>) {}
+	async create(recipe: Recipe) {
+		return await this.recipeRepository.insert(recipe);
+	}
 }
