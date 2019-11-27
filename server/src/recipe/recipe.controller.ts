1b93285d65fcf904720af43ae2bce2ea98d60271
ericafenyo <ericafenyo@yahoo.com>
2019-11-27 18:02:18 +0100

    :zap: update recipe crud operation

--- server/src/recipe/recipe.controller.ts
+++ server/src/recipe/recipe.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	NotFoundException,
+	BadRequestException,
+	UseInterceptors,
+	UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { RecipeService } from './recipe.service';
+import { Recipe } from './recipe.entity';
 
-@Controller('recipe')
+@Controller('recipes')
 export class RecipeController {
-  constructor(private recipeService: RecipeService) {}
+	constructor(private recipeService: RecipeService) {}
+
+	@Post()
+	@UseInterceptors(FileInterceptor('image'))
+	async create(@Body() recipe, @Param() params, @UploadedFile() file) {
+		console.log(recipe);
+		console.log(file);
+
+		const recipeConstruct = { ...recipe, userId: params.userId };
+		try {
+			// const result = await this.recipeService.create(recipeConstruct);
+			// console.log(result);
+			// if (!result) {
+			// 	throw new NotFoundException();
+			// }
+		} catch (error) {
+			console.log(error);
+			throw new BadRequestException();
+		}
+	}
 }
