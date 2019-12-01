ef8e88297a6fead6a70d912444c89a16ea581fe4
ericafenyo <ericafenyo@yahoo.com>
2019-12-01 19:12:37 +0100

    :zap:  update app module with submodules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,24 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user/user.controller';
-import { RecipeModule } from './recipe/recipe.module';
+import { IngredientModule } from './ingredient/ingredient.module';
+import { DirectionModule } from './direction/direction.module';
+import { RecipeMetadataModule } from './recipe-metadata/recipe-metadata.module';
+import { ProfileModule } from './profile/profile.module';
+import { PhotoModule } from './photo/photo.module';
 
 @Module({
-	imports: [ AuthModule, UserModule, TypeOrmModule.forRoot(), RecipeModule ],
-	controllers: [ AppController ],
-	providers: [ AppService ]
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot(),
+    IngredientModule,
+    DirectionModule,
+    RecipeMetadataModule,
+    ProfileModule,
+    PhotoModule
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ]
 })
 export class AppModule {}
