ceb47292685c787db5d47570755cb5f1815599f1
ericafenyo <ericafenyo@yahoo.com>
2019-12-09 07:18:25 +0100

    :zap: improve image cropping

--- app/src/components/PhotoElements/PhotoPicker/PhotoPicker.vue
+++ app/src/components/PhotoElements/PhotoPicker/PhotoPicker.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="fluid">
-    <!-- <GlobalPopup v-if="showImageCropper" noCloseButton>
-      <ImageCropper :imageUrl="imagePickedFromFileSystemUrl" class="cropper" @cropConfirmed="handleCropConfirmation" />
-    </GlobalPopup>-->
+    <Overlay :isOpen="showImageCropper">
+      <ImageCropper
+        :imageUrl="imagePickedFromFileSystemUrl"
+        class="cropper"
+        @crop-confirmed="handleCropConfirmation"
+      />
+    </Overlay>
 
     <div
       class="image-frame fluid"
@@ -10,8 +14,10 @@
       @click="handleEmptyFrameClick"
     >
       <template v-if="selectedImageUrl">
-        <div class="delete-button" @click.stop="handleDeletePhotoClick" />
-        <img :src="selectedImageUrl" class="image" />
+        <div class="delete-button" @click.stop="handleDeletePhotoClick">
+          <Icon name="delete"/>
+        </div>
+        <img :src="selectedImageUrl" class="fluid" />
       </template>
       <template v-else>
         <Icon name="add" width="32px" height="32px" />
@@ -32,14 +38,14 @@
 <script lang="ts">
 import { Vue, Component, Prop, Emit, Watch } from "vue-property-decorator";
 import Icon from "@/components/Icons/Icon.vue";
-// import GlobalPopup from '@transway/nsc-popup-global-popup/GlobalPopup.vue';
-// import ImageCropper from './ImageCropper.vue';
+import Overlay from "@/components/Overlay/Overlay.vue";
+import ImageCropper from "@/components/PhotoElements/PhotoPicker/ImageCropper.vue";
 
 @Component({
   components: {
-    Icon
-    // ImageCropper,
-    // GlobalPopup
+    Icon,
+    ImageCropper,
+    Overlay
   }
 })
 export default class PhotoPicker extends Vue {
@@ -83,13 +89,9 @@ export default class PhotoPicker extends Vue {
   }
 
   handleFileSelection(file) {
-
-    
     if (file) {
       const urlCreator = window.URL || (window as any).webkitURL;
       this.imagePickedFromFileSystemUrl = urlCreator.createObjectURL(file);
-      console.log(this.imagePickedFromFileSystemUrl);
-      
       this.showImageCropper = true;
     }
     (this.$refs.inputFile as HTMLInputElement).value = "";
@@ -103,7 +105,7 @@ export default class PhotoPicker extends Vue {
   }
 
   handleImageChange() {
-    this.$emit("imageChanged", this.selectedImageUrl);
+    this.$emit("image-changed", this.selectedImageUrl);
   }
 }
 </script>
@@ -139,17 +141,12 @@ export default class PhotoPicker extends Vue {
 }
 
 .delete-button {
-  width: 16px;
-  height: 16px;
   position: absolute;
-  display: block;
-  top: 0;
-  right: 0;
-  margin-top: 5px;
-  margin-right: 5px;
+  top: 4px;
+  right: 4px;
 }
 .image {
-  max-width: 100%;
-  max-height: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
