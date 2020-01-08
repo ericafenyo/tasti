5474d1e4613642fc05ca5b2adf78420502bb35f1
ericafenyo <ericafenyo@yahoo.com>
2020-01-08 07:39:57 +0100

    remove multer file upload

--- server/src/recipe/recipe.module.ts
+++ server/src/recipe/recipe.module.ts
@@ -3,31 +3,13 @@ import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Recipe } from './recipe.entity';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { User } from '../user/user.entity';
 import { RecipeMetadataModule } from '../recipe-metadata/recipe-metadata.module';
-const generator = require('generate-password');
 
-const path = require('path');
 @Module({
   imports: [
     TypeOrmModule.forFeature([ Recipe, User ]),
-    RecipeMetadataModule,
-    MulterModule.register({
-      storage: diskStorage({
-        destination: './uploads',
-        filename(_, file, callback) {
-          const baseName = generator.generate({
-            length: 12,
-            numbers: true,
-            excludeSimilarCharacters: true,
-          });
-          const fileExtension = path.extname(file.originalname);
-          callback(null, baseName + fileExtension);
-        },
-      }),
-    }),
+    RecipeMetadataModule
   ],
 
   controllers: [ RecipeController ],
