7293999dc812c371c6308e442784995ac2094fab
ericafenyo <ericafenyo@yahoo.com>
2020-01-28 10:04:00 +0100

    update vuex store

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { userRepository, recipeService } from '@/data/Injector';
+import { User, Recipe } from '@/data/Injector';
 import { RecipeRequestModel } from '@/data/recipe/recipe.model';
+import { RecipeService } from '@/data/recipe/recipe.service';
 
 Vue.use(Vuex);
 
 const initialState = {
+  isModalOpen: false,
   users: [],
   token: ''
 };
@@ -22,19 +24,26 @@ export const store = new Vuex.Store({
 
   actions: {
     createUser({ commit }, userInfo: object) {
-      return userRepository.createAccount(userInfo);
+      return User.createAccount(userInfo);
     },
 
     async authenticate({ commit }, { username, password }) {
-      const response = await userRepository.authenticate(username, password);
+      const response = await User.authenticate(username, password);
       commit('setToken', response.data.access_token);
       return response;
     },
 
     createRecipe(_, recipeInfo: RecipeRequestModel) {
-      const response = recipeService.create(recipeInfo);
+      const response = Recipe.create(recipeInfo);
       console.log(response);
-      
+    },
+
+    getProfile() {
+      return User.profile();
+    },
+
+    getRecipes() {
+      return Recipe.find();
     }
   }
 });
