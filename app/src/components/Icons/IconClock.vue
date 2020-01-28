b3c313da731edd6e27f39c966e5206e892bf5127
ericafenyo <ericafenyo@yahoo.com>
2020-01-28 10:04:12 +0100

    add new icon

---
+++ app/src/components/Icons/IconClock.vue
@@ -0,0 +1,8 @@
+<template>
+  <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.6 0 12 0zm6 13.5h-7.5V6h3v4.5H18v3z"/>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
