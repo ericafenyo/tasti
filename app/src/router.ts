ada9d6726bdaed1ae480d3cac8e1fa96b3576089
ericafenyo <ericafenyo@yahoo.com>
2020-01-28 09:59:23 +0100

    add route

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Home from './views/Home/Home.vue';
 import Login from './views/Login/Login.vue';
 import Register from './views/Register/Register.vue';
 import CreateRecipe from './views/CreateRecipe/CreateRecipe.vue';
-import UserProfile from './views/UserProfile/UserProfile.vue'
+import UserProfile from './views/UserProfile/UserProfile.vue';
 
 Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: Home
+    },
     {
       path: '/login',
       component: Login
