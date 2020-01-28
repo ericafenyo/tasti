e0d835b61ba19322194ccbe3482947a5823e442f
ericafenyo <ericafenyo@yahoo.com>
2020-01-28 10:03:38 +0100

    add a CRUD service object

--- app/src/data/Service.ts
+++ app/src/data/Service.ts
@@ -1,5 +1,10 @@
 import { CreatedResponse } from '@/types';
 
-export interface Service{
+/**
+ * A Service encapsulates methods used to access and mutate objects.
+ * ind, create, update, delete, etc.
+ */
+export interface Service {
   create(param?: any): CreatedResponse;
+  find(): any;
 }
