38fa9fbe227ecd7865dd3e83d7a616c99778cdc9
ericafenyo <ericafenyo@yahoo.com>
2020-02-03 12:46:53 +0100

    update vue actions

--- app/src/store/actions.ts
+++ app/src/store/actions.ts
@@ -1,6 +1,8 @@
 export const Actions = {
   GET_PROFILE: 'getProfile',
   GET_RECIPES: 'getRecipes',
+  GET_FOLLOWING: 'getFollowing',
+  GET_FOLLOWERS: 'getFollowers',
   CREATE_RECIPE: 'createRecipe',
   CREATE_USER: 'createUser'
 };
