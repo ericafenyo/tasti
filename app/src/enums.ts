3952c6c074e716038ff952400f1fb487cf16d0a9
ericafenyo <ericafenyo@yahoo.com>
2020-02-05 23:39:16 +0100

    upadate fields

--- app/src/enums.ts
+++ app/src/enums.ts
@@ -1,7 +1,9 @@
 export enum HttpStatus {
   CREATED = 201,
-  UNAUTHORIZED = 401
+  UNAUTHORIZED = 401,
+  SERVICE_UNAVAILABLE = 503
 }
 export enum AlertKeys {
-  INVALID_CREDENTIALS = 'invalid-credentials'
+  INVALID_CREDENTIALS = 'invalid-credentials',
+  SERVICE_UNAVAILABLE = 'service-unavailable'
 }
