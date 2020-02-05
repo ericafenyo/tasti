36f9db0257e6123633796b79f8a9fc0bff7cd51e
ericafenyo <ericafenyo@yahoo.com>
2020-02-05 23:41:07 +0100

    :construction: tweak components

--- app/src/components/Link/Link.vue
+++ app/src/components/Link/Link.vue
@@ -1,6 +1,12 @@
 <template>
   <router-link :to="to" v-slot="{ href, route, navigate, isActive, isExactActive }">
-    <a class="link" v-html="text" :active="isActive" :href="href" @click="navigate" />
+    <a
+      :class="['link', (`link--${size}`)]"
+      v-html="text"
+      :active="isActive"
+      :href="href"
+      @click="navigate"
+    />
   </router-link>
 </template>
 
@@ -11,6 +17,7 @@ import { Vue, Prop, Emit, Component } from "vue-property-decorator";
 export default class Link extends Vue {
   @Prop({ type: String, default: "Link" }) text: string;
   @Prop({ type: String, default: "#" }) to: string;
+  @Prop({ type: String, default: "medium" }) size: string;
 }
 </script>
 
@@ -18,11 +25,23 @@ export default class Link extends Vue {
 @import "@/scss/_resources.scss";
 
 .link {
-  color: $color-accent;
-  text-decoration: none;
+  color: $color-primary-text;
+  font-weight: 500;
+
+  &--small {
+    font-size: 0.875rem;
+  }
+
+  &--medium {
+    font-size: 1rem;
+  }
+
+  &--large {
+    font-size: 1.2rem;
+  }
 
   &:hover {
-    text-decoration: underline;
+    color: $color-accent;
   }
 }
 </style>
