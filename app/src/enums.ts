04e8d83d8f99b46ce0da4753276a41d59020566c
ericafenyo <ericafenyo@yahoo.com>
2020-02-09 16:28:56 +0100

    fze

--- app/src/enums.ts
+++ app/src/enums.ts
@@ -4,9 +4,3 @@ export enum HttpStatus {
   SERVICE_UNAVAILABLE = 503,
   CONFLICT = 409
 }
-export enum AlertKeys {
-  INVALID_CREDENTIALS = 'invalid-credentials',
-  SERVICE_UNAVAILABLE = 'service-unavailable',
-  REGISTRATION_SUCCESSFUL = 'registration_successful',
-  DUPLICATE_EMAIL = 'duplicate-email'
-}
