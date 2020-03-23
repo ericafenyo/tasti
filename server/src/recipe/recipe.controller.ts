0f4fb2ce3c25054c5a950244b751b84407fa6c1e
ericafenyo <ericafenyo@yahoo.com>
2020-03-23 01:23:11 +0100

    update user api

--- server/src/recipe/recipe.controller.ts
+++ server/src/recipe/recipe.controller.ts
@@ -12,40 +12,34 @@ import {
   UploadedFiles
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { FileInterceptor, FileFieldsInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { RecipeService } from './recipe.service';
-import { RecipeMetadataService } from '../recipe-metadata/recipe-metadata.service';
 import { RecipeDto } from './recipe.dto';
 import { multerOption } from '../core';
 import { AuthType } from 'src/enums';
+import { CurrentUser, CurrentUserInfo } from '../auth/user.decorator';
+import { FilesDto } from 'src/photo/photo.dto';
 
 @Controller('recipes')
 export class RecipeController {
-  constructor(private recipeService: RecipeService, private metadataService: RecipeMetadataService) {}
+  constructor(private recipeService: RecipeService) { }
 
+  // Create a recipe
   @Post()
   @UseGuards(AuthGuard(AuthType.JWT))
-  @UseInterceptors(FileInterceptor('image', multerOption))
-  async createRecipe(@Request() request: any, @UploadedFile() file) {
-    const { user, body: { name } } = request;
-    const imagePath =
-      file ? file.key :
-      '';
-    const recipe: RecipeDto = { name, imagePath };
-    console.log(recipe);
-
-    return await this.recipeService.create(user.id, recipe);
+  @UseInterceptors(FileFieldsInterceptor(
+    [
+      { name: "image", maxCount: 1 },
+      { name: "photos", maxCount: 4 }
+    ],
+    multerOption
+  ))
+  async createRecipe(
+    @CurrentUser() user: CurrentUserInfo,
+    @Body() recipe: RecipeDto,
+    @UploadedFiles() files: FilesDto) {
+    return await this.recipeService.create(user, recipe, files);
   }
-
-  @Post(':id/metadata')
-  async createRecipeMetadata(@Body() body: any, @Param('id') recipeId: string) {
-    return await this.metadataService.create(recipeId, body);
-  }
-
-  // @Get(':id')
-  // async getRecipeById(@Param('id') recipeId: string) {
-  //   return await this.recipeService.findOne(recipeId);
-  // }
 
   @Get()
   async getRecipes() {
--- server/src/recipe/recipe.module.ts
+++ server/src/recipe/recipe.module.ts
@@ -3,15 +3,15 @@ import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Recipe } from './recipe.entity';
-import { User } from '../user/user.entity';
-import { RecipeMetadataModule } from '../recipe-metadata/recipe-metadata.module';
 import { Ingredient } from '../ingredient/ingredient.entity';
 import { Direction } from '../direction/direction.entity';
+import { UserModule } from '../user/user.module';
+import { Photo } from '../photo/photo.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Recipe, User, Ingredient, Direction]),
-    RecipeMetadataModule
+    UserModule,
+    TypeOrmModule.forFeature([Recipe, Ingredient, Direction, Photo])
   ],
 
   controllers: [RecipeController],
