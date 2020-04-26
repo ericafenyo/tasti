c56dd5d0b8f068551e92c0448adfaaac22611b6c
ericafenyo <ericafenyo@yahoo.com>
2020-04-26 08:57:32 +0200

    add migration settings

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,11 +1,16 @@
 module.exports = {
-	type: "mysql",
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE_NAME,
-	entities: ["dist/**/*.entity{.ts,.js}"],
-	synchronize: true,
-	logging: true
-}
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  logging: ['error', 'schema', 'query'],
+  migrations: ['dist/migration/**/*{.ts,js}'],
+  migrationsRun: true,
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+};
