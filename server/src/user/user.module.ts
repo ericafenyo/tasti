8a8b0b5998ed05db65bef971f9d2f90df5679799
ericafenyo <ericafenyo@yahoo.com>
2020-04-26 08:59:43 +0200

    add credential orm config to auth module

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { Profile } from 'src/profile/profile.entity';
+import { Profile } from '../profile/profile.entity';
+import { Credential } from '../auth/credential.entity';
 
 @Module({
-  imports: [  TypeOrmModule.forFeature([ User, Profile ]) ],
+  imports: [  TypeOrmModule.forFeature([ User, Profile, Credential ]) ],
   providers: [ UserService],
   exports: [ UserService ],
   controllers: [ UserController ],
