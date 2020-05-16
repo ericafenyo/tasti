719c3e81f18ccba0d54cd8ff7810d5451b3332b0
ericafenyo <ericafenyo@yahoo.com>
2020-05-16 21:42:51 +0200

    :constuction: WIP

---
+++ app/src/screens/Recipe/RecipeItem.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="recipe-item">
+    <div>
+        <div>
+            <Avatar/>
+            <span>Ana Smith</span>
+        </div>
+        <div class="aspect-ratio">
+            <img src="" alt="">
+        </div>
+        <div>
+            <h3>Buckweat noodles</h3>
+            <p>Sooo easy to make and double or triple needed if needed. Serve as the main meal with a salad and crusty bread, or as a side dish.</p>
+
+        </div>
+    </div>
+
+</div>
+  
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator';
+export default class RecipeItem extends Vue{
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_resources.scss";
+
+
+</style>
