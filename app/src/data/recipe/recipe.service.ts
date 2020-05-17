20b77a3a36790b62830322ff1a59212d5cd871dc
ericafenyo <ericafenyo@yahoo.com>
2020-05-17 22:21:58 +0200

    get recipes

--- app/src/data/recipe/recipe.service.ts
+++ app/src/data/recipe/recipe.service.ts
@@ -1,17 +1,17 @@
 import { Service } from '../Service';
-import { buildRequest, authHttp } from '../ConnectionHelper';
+import { buildRequest, authHttp, http } from '../ConnectionHelper';
 import { AxiosPromise } from 'axios';
-import { RecipeUiModel } from './recipe.model';
+import { RecipeResponse } from './recipe.model';
 import { Result } from '../Result';
 import { HttpStatus } from '@/enums';
 
 export class RecipeService implements Service {
   /**
    * Get a single recipe
-   * 
+   *
    * @param {String} id - the id of the recipe to fetch
-   * 
-   * @returns a {@link Result} object containing the http status code and the data. 
+   *
+   * @returns a {@link Result} object containing the http status code and the data.
    */
   findOne(id: string) {
     return buildRequest(() => authHttp().get(`/recipes/${id}`));
@@ -20,39 +20,36 @@ export class RecipeService implements Service {
   private async upload(files: any) {
     const config = {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
+        'Content-Type': 'multipart/form-data',
+      },
     };
 
-    return buildRequest(() => authHttp().post('/upload', files, config))
+    return buildRequest(() => authHttp().post('/upload', files, config));
   }
 
   async create(payload: any): Promise<Result> {
     const { data, status } = await this.upload(payload.files);
-    let requestModel = { ...payload.inputData }
+    let requestModel = { ...payload.inputData };
 
     if (status === HttpStatus.CREATED) {
       const { image, photos } = data;
-      requestModel = { ...requestModel, imagePath: image[0], photos }
+      requestModel = { ...requestModel, imagePath: image[0], photos };
     } else {
-      requestModel = { ...requestModel, imagePath: "", photos: [] }
+      requestModel = { ...requestModel, imagePath: '', photos: [] };
     }
 
     const config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     };
 
-    return buildRequest(() => authHttp().post('/recipes', requestModel, config));
+    return buildRequest(() =>
+      authHttp().post('/recipes', requestModel, config)
+    );
   }
 
-  find(): AxiosPromise<RecipeUiModel> {
-    return authHttp().get('/recipes').then((response) =>
-      response.data.map((element: any) => ({
-        ...element,
-        image: process.env.VUE_APP_IMAGE_BASE_URL + element.imagePath
-      }))
-    );
+  async find(): Promise<Result> {
+    return buildRequest(() => http.get('/recipes'));
   }
 }
