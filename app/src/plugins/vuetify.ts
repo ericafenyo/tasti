28aaed9435d3743df360bcb7fafe7ee9e976c079
ericafenyo <ericafenyo@yahoo.com>
2020-06-11 02:01:46 +0200

    wip: define vuetify theme

--- app/src/plugins/vuetify.ts
+++ app/src/plugins/vuetify.ts
@@ -1,7 +1,19 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: "#4299e1",
+        surface: "#eff3f6",
+        accent: "#4299e1",
+      },
+    },
+  },
 });
